a3f1282445f149354e7167d7966b577922102ddd
Ilochi Gabriel <gabrielilo190@gmail.com>
2021-02-12 10:02:13 -1200

    Add pages routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\pages\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group([''], function () {
+    Route::get('vision', [PageController::class, 'vision'])->name('about.vision');
+    Route::get('careers', [PageController::class, 'careers'])->name('pages.careers');
+    Route::get('career-detail', [PageController::class, 'careerDetail'])->name('pages.career-detail');
+    Route::get('contact', [PageController::class, 'contact'])->name('pages.contact');
+    Route::get('events', [PageController::class, 'events'])->name('pages.events');
+    Route::get('event-detail', [PageController::class, 'eventDetail'])->name('pages.event-detail');
+    Route::get('faculty-detail', [PageController::class, 'facultyDetail'])->name('pages.faculty-detail');
+    Route::get('faculties', [PageController::class, 'faculties'])->name('pages.faculties');
+    Route::get('faq', [PageController::class, 'faq'])->name('pages.faq');
+    Route::get('/', [PageController::class, 'index'])->name('pages.index');
+    Route::get('privacy', [PageController::class, 'privacy'])->name('pages.privacy');
+    Route::get('support', [PageController::class, 'support'])->name('pages.support');
 });
