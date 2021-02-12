7f64503d2049b8bc9c7eca98d8d93e269c9b58b7
mikky5n <jesmikky@gmail.com>
2021-02-13 00:23:54 +0100

    DB migrations

---
+++ database/migrations/2021_02_12_224058_create_staff_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->integer('staff_list_id')->nullable();
+            $table->string('publications')->nullable();
+            $table->string('qualifications')->nullable();
+            $table->string('cv')->nullable();
+            $table->string('email')->nullable();
+            $table->string('research_areas')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff_profiles');
+    }
+}
