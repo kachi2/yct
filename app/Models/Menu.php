784c61f4412870ee4ca76f8fd5668982372f799b
Ilochi Gabriel <gabrielilo190@gmail.com>
2021-02-16 08:59:44 +0000

    Fix conflict

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -5,11 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-<<<<<<< HEAD:app/saved/Menu.php
 class Menu extends Model
-=======
-class Unit extends Model
->>>>>>> 69fe9ed31a7ed06813012594833488cb71acb9be:app/Models/Unit.php
 {
-    use HasFactory;
+  use HasFactory;
+
 }
