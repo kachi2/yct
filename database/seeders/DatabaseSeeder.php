84ed91342ba571c3768478563b7f802d1ba0908d
Ilochi Gabriel <gabrielilo190@gmail.com>
2021-03-02 05:02:18 +0000

    reorder seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            MenuSeeder::class,
             NavListSeeder::class,
+            MenuSeeder::class,
             NavDropdownSeeder::class
         ]);
     }
