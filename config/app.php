01cd95b887c7da3ef049eb93f73f48eecdbf2d5d
Ilochi Gabriel <gabrielilo190@gmail.com>
2021-03-11 15:06:18 +0000

    add base domain

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -230,5 +230,6 @@ return [
     ],
 
     'log' => 'errorlog',
+    'base_domain' => env('APP_BASE_DOMAIN', 'yabatech.edu.eng')
 
 ];
