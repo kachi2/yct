df096ee217ff6a13b64f95e1a0c3869a4b63db25
mikky5n <jesmikky@gmail.com>
2021-03-12 10:17:01 +0100

    new updates

--- resources/views/pages/career-detail.blade.php
+++ resources/views/pages/career-detail.blade.php
@@ -9,13 +9,13 @@
         style="background: url({{ asset('./assets/img/site/categ.jpg') }}) no-repeat"
     >
         <div class="container">
-            <h2 class="text-white">Career</h2>
+            <h2 class="text-white">Pages</h2>
             <ol
                 class="breadcrumb breadcrumb-double-angle text-white bg-transparent p-0"
             >
                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item"><a href="#">Careers</a></li>
-                <li class="breadcrumb-item">Job Apply</li>
+                <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                <li class="breadcrumb-item">Description</li>
             </ol>
         </div>
     </div>
@@ -26,7 +26,7 @@
                 <div class="col-lg-9 col-md-8">
                     <div class="card padding-30 shadow-v1 marginTop-30">
                         <div class="mb-5">
-                            <h4 class="mb-3">Job Description</h4>
+                            <h4 class="mb-3">Description</h4>
                             <p>
                                 Nam liber tempor cum soluta nobis eleifend option congue is
                                 nihil imper per tem por legere me that doming vulputate the
@@ -41,7 +41,7 @@
                         </div>
 
                         <div class="mb-5">
-                            <h4 class="mb-3">Responsibilities</h4>
+                            <h4 class="mb-3"></h4>
                             <p>
                                 Nam liber tempor cum soluta nobis eleifend option congue is
                                 nihil imper per tem por legere me that doming vulputate the
@@ -69,7 +69,7 @@
                         </div>
 
                         <div class="mb-5">
-                            <h4 class="mb-3">Minimum Qualifications</h4>
+                            <h4 class="mb-3"></h4>
                             <ul class="list-unstyled list-style-icon list-icon-bullet">
                                 <li>
                                     Build next-generation web applications with a focus on the
@@ -98,7 +98,7 @@
                         </div>
 
                         <div class="mb-5">
-                            <h4 class="mb-3">Bonus Points</h4>
+                            <h4 class="mb-3"></h4>
                             <ul class="list-unstyled list-style-icon list-icon-bullet">
                                 <li>
                                     Build next-generation web applications with a focus on the
@@ -135,113 +135,26 @@
                 </div>
                 <div class="col-lg-3 col-md-4">
                     <div class="card padding-30 shadow-v1 marginTop-30">
-                        <h4 class="mb-3">Job Features</h4>
+                        <h4 class="mb-3">Quick Links</h4>
                         <ul class="list-unstyled">
                             <li class="mb-3">
-                                <i class="fas fa-map-marker-alt mr-1"></i>
-                                <span class="font-weight-semiBold">Location:</span> <br/>
-                                Yabatech, Lagos
+                                Chairman Governing Council
                             </li>
                             <li class="mb-3">
-                                <i class="fas fa-briefcase mr-1"></i>
-                                <span class="font-weight-semiBold">Job Type:</span> <br/>
-                                Full time
+                               Deputy Rector Academics
                             </li>
                             <li class="mb-3">
-                                <i class="fas fa-dollar-sign mr-1"></i>
-                                <span class="font-weight-semiBold">Salary: </span> <br/>
-                                ₦47,000 - ₦102,000
+                                Deputy Rector Admin
+                                
                             </li>
                             <li class="mb-3">
-                                <i class="fas fa-graduation-cap"></i>
-                                <span class="font-weight-semiBold">Education:</span> <br/>
-                                Master or Bachelor degre
+                             Registrar
                             </li>
                             <li class="mb-3">
-                                <i class="fas fa-flask mr-1"></i>
-                                <span class="font-weight-semiBold">Experience:</span> <br/>
-                                2+ years
+                             Polytechnic Librarian
                             </li>
                         </ul>
                     </div>
-                </div>
-            </div>
-            <!-- END row-->
-        </div>
-        <!-- END container-->
-    </section>
-
-    <section class="padding-y-120">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-6 mx-auto text-center">
-                    <h2>Apply For This Job</h2>
-                    <div
-                        class="width-3rem height-4 rounded bg-primary mx-auto mb-4"
-                    ></div>
-                    <p>
-                        Prepare a PDF for your resume and fill the following form. We will
-                        contact you as soon as possible.
-                    </p>
-                </div>
-                <div class="col-lg-9 mx-auto mt-5">
-                    <form class="row" action="#" method="POST">
-                        <div class="col-md-6 mb-4">
-                            <input
-                                type="text"
-                                class="form-control rounded-pill"
-                                placeholder="First Name"
-                                required=""
-                            />
-                        </div>
-                        <div class="col-md-6 mb-4">
-                            <input
-                                type="text"
-                                class="form-control rounded-pill"
-                                placeholder="Last Name"
-                            />
-                        </div>
-                        <div class="col-md-6 mb-4">
-                            <input
-                                type="email"
-                                class="form-control rounded-pill"
-                                placeholder="Email"
-                                required=""
-                            />
-                        </div>
-                        <div class="col-md-6 mb-4">
-                            <input
-                                type="text"
-                                class="form-control rounded-pill"
-                                placeholder="Phone"
-                            />
-                        </div>
-                        <div class="col-md-6 mb-4">
-                            <input
-                                type="text"
-                                class="form-control rounded-pill"
-                                placeholder="Website"
-                            />
-                        </div>
-                        <div class="col-md-6 mb-4">
-                            <input
-                                type="text"
-                                class="form-control rounded-pill"
-                                placeholder="Location"
-                            />
-                        </div>
-                        <div class="col-12 mb-4">
-                <textarea
-                    type="text"
-                    class="form-control rounded-pill"
-                    rows="5"
-                    placeholder="More Info"
-                ></textarea>
-                        </div>
-                        <div class="col-12 mb-5 text-center">
-                            <button class="btn btn-primary">Submit Now</button>
-                        </div>
-                    </form>
                 </div>
             </div>
             <!-- END row-->
